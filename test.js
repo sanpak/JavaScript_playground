235f5fecd1f9b3f3f0c3aafbddd19cd77b4968ad
SANPAK Ng <pakeng369@gmail.com>
2019-09-07 21:06:34 -0400

    console log

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
-let total = 0,count = 1;
-while (count <= 10) {
-  total += count;
-  count += 1;
-}
-
-console.log(total);
+// let total = 0,count = 1;
+// while (count <= 10) {
+//   total += count;
+//   count += 1;
+// }
+//
+// console.log(total);
+// console.log(100 + 4)
+// console.log(0 / 0)
+// console.log("Down on the sea ")
+// console.log('Lie on the Ocean " sda')
+// console.log("Hello\thello\n\thello")
+console.log("A newline character is written like \"\\n\".")
+console.log("con" + "cat" + "e" + "nate")
+console.log(`half of 100 is ${100/2}`)
