212a31086be255aa399459aee2ab75491aa40cec
SANPAK Ng <pakeng369@gmail.com>
2019-09-10 22:30:33 -0400

    Chapter2

--- test.js
+++ test.js
@@ -1,32 +1,48 @@
-// let total = 0,count = 1;
-// while (count <= 10) {
-//   total += count;
-//   count += 1;
-// }
-//
-// console.log(total);
-// console.log(100 + 4)
-// console.log(0 / 0)
-// console.log("Down on the sea ")
-// console.log('Lie on the Ocean " sda')
-// console.log("Hello\thello\n\thello")
-// console.log("A newline character is written like \"\\n\".")
-// console.log("con" + "cat" + "e" + "nate")
-// console.log(`half of 100 is ${100/2}`)
-// console.log(typeof 4.5)
-// console.log(typeof "x")
-// console.log(3 > 2)
-// console.log(3 < 2)
-// console.log("Aardivark" < "zoo")
-// console.log("Z" < "z")
-// console.log("Z" < "a")
-// console.log("Itchy" != "Scasda")
-// console.log("itchy" == "itchy")
-// console.log("Itchy" == "itchy")
-// console.log(NaN == NaN)
-// console.log(false && false)
-// console.log(true && true)
-// console.log(false || true)
-// console.log(true || false)
-// console.log(!true)
-// console.log(1 > 2 && 100 < 50 || 100 > 1)
+// // // let total = 0,count = 1;
+// // // while (count <= 10) {
+// // //   total += count;
+// // //   count += 1;
+// // // }
+// // //
+// // // console.log(total);
+// // // console.log(100 + 4)
+// // // console.log(0 / 0)
+// // // console.log("Down on the sea ")
+// // // console.log('Lie on the Ocean " sda')
+// // // console.log("Hello\thello\n\thello")
+// // // console.log("A newline character is written like \"\\n\".")
+// // // console.log("con" + "cat" + "e" + "nate")
+// // // console.log(`half of 100 is ${100/2}`)
+// // // console.log(typeof 4.5)
+// // // console.log(typeof "x")
+// // // console.log(3 > 2)
+// // // console.log(3 < 2)
+// // // console.log("Aardivark" < "zoo")
+// // // console.log("Z" < "z")
+// // // console.log("Z" < "a")
+// // // console.log("Itchy" != "Scasda")
+// // // console.log("itchy" == "itchy")
+// // // console.log("Itchy" == "itchy")
+// // // console.log(NaN == NaN)
+// // // console.log(false && false)
+// // // console.log(true && true)
+// // // console.log(false || true)
+// // // console.log(true || false)
+// // // console.log(!true)
+// // // console.log(1 > 2 && 100 < 50 || 100 > 1)
+// // // console.log(true? 5: 1)
+// // // console.log(false? 6: 2)
+// // // console.log(false || "John")
+// // if ( 1 || 0 ) {
+// //   console.log("true")
+// //
+// // } else {
+// //   console.log("false")
+// // }
+// console.log(1 || 0);
+// console.log(typeof("abc"))
+1;
+!false;
+
+let ten = 10;
+console.log(ten * ten)
