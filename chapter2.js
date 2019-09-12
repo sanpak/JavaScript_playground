8c02fd9fd1f35902ad2ea8852655ddff223e2f9a
SANPAK Ng <pakeng369@gmail.com>
2019-09-12 19:33:16 -0400

    trying loop

--- chapter2.js
+++ chapter2.js
@@ -1,10 +1,34 @@
-//while loop 2 to the 10th power
-let result = 1;
-let counter = 0;
-
-while (counter < 10) {
-  result *= 2;
-  counter += 1;
-}
-
-console.log(result);
+// // // // // //while loop 2 to the 10th power
+// // // // // let result = 1;
+// // // // // let counter = 0;
+// // // // //
+// // // // // while (counter < 10) {
+// // // // //   result *= 2;
+// // // // //   counter += 1;
+// // // // // }
+// // // // //
+// // // // // console.log(result);
+// // // // let yourName;
+// // // //
+// // // // do {
+// // // //   yourName = prompt("Who are you?");
+// // // // } while (!yourName);
+// // // // console.log(yourName);
+// // // if (false != true) {
+// // //   console.log("That makes sense.");
+// // //   if (1 < 2) {
+// // //     console.log("No surprise there.");
+// // //   }
+// // // }
+// // for (let number = 0; number <= 12; number += 2){
+// //   console.log(number);
+// // }
+// let result = 1;
+// for (let counter = 0; counter < 10; counter += 1){
+//   result *= 2;
+// }
+//
+// console.log(result);
+//
+//
+for (let current = 20;)
