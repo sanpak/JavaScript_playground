1fe8a18c1c980d017a2df325be4481883c63b9a8
SANPAK Ng <pakeng369@gmail.com>
2019-09-11 22:16:21 -0400

    eloquent JavaScript Ch.2

---
+++ chapter2.js
@@ -0,0 +1,10 @@
+//while loop 2 to the 10th power
+let result = 1;
+let counter = 0;
+
+while (counter < 10) {
+  result *= 2;
+  counter += 1;
+}
+
+console.log(result);
--- test.js
+++ test.js
@@ -46,3 +46,11 @@
 
 let ten = 10;
 console.log(ten * ten)
+
+let mood = "light";
+console.log(mood);
+mood = "dark";
+console.log(mood);
+let luigisDebt = 140;
+luigisDebt -= 35;
+console.log(luigisDebt);
