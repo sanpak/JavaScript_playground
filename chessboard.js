f8bf18c3b7175503c5cc537191e6e15132259cce
SANPAK Ng <pakeng369@gmail.com>
2019-09-15 23:06:19 -0400

    fizzbuzz version 2

--- chessboard.js
+++ chessboard.js
@@ -16,18 +16,32 @@
 // }
 
 //version 2
-board = "";
-size = 8;
-
-for (row = 0; row < size; row += 1){
-  for (width = 0; width < size / 2; width += 1){
-    if (row % 2 == 0) {
-      board += " #";
-
+// board = "";
+// size = 8;
+//
+// for (row = 0; row < size; row += 1){
+//   for (width = 0; width < size / 2; width += 1){
+//     if (row % 2 == 0) {
+//       board += " #";
+//
+//     } else {
+//       board += "# ";
+//     }
+//   }
+//   console.log(board);
+//   board = "\n";
+// }
+//book's version
+let board = "";
+let size = 8;
+for (let row = 0; row < size; row++) {
+  for (let width = 0; width < size; width++) {
+    if ((row + width) % 2 == 0){
+      board += " ";
     } else {
-      board += "# ";
+      board += "#";
     }
   }
-  console.log(board);
-  board = "\n";
+  board += "\n"
 }
+console.log(board);
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,27 @@
 
-for (n = 1; n <= 100; n += 1) {
-  if (n % 5 == 0 && n % 3 ==0) {
-    console.log("FizzBuzz");
-  } else if (n % 3 == 0) {
-      console.log("Fizz");
-    } else if (n % 5 == 0 && n % 3 != 0) {
-      console.log("Buzz");
-    } else{
-      console.log(n);
+// for (n = 1; n <= 100; n += 1) {
+//   if (n % 5 == 0 && n % 3 ==0) {
+//     console.log("FizzBuzz");
+//   } else if (n % 3 == 0) {
+//       console.log("Fizz");
+//     } else if (n % 5 == 0 && n % 3 != 0) {
+//       console.log("Buzz");
+//     } else{
+//       console.log(n);
+//     }
+// }
+//version 2
+word = "";
+for (n = 1; n <= 100; n++) {
+  if (n % 3 == 0) {
+    word = "Fizz";
+    if (n % 5 == 0) {
+      word += "Buzz";
     }
+  } else if (n % 5 == 0) {
+    word = "Buzz";
+  } else {
+    word = n;
+  }
+  console.log(word);
 }
