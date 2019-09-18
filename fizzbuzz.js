7ca4ddc446d57d6e3b3a49be3e8593a5ac95fe81
SANPAK Ng <pakeng369@gmail.com>
2019-09-18 13:44:50 -0400

    nexted scope

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -11,17 +11,25 @@
 //     }
 // }
 //version 2
-word = "";
-for (n = 1; n <= 100; n++) {
-  if (n % 3 == 0) {
-    word = "Fizz";
-    if (n % 5 == 0) {
-      word += "Buzz";
-    }
-  } else if (n % 5 == 0) {
-    word = "Buzz";
-  } else {
-    word = n;
-  }
-  console.log(word);
+// word = "";
+// for (n = 1; n <= 100; n++) {
+//   if (n % 3 == 0) {
+//     word = "Fizz";
+//     if (n % 5 == 0) {
+//       word += "Buzz";
+//     }
+//   } else if (n % 5 == 0) {
+//     word = "Buzz";
+//   } else {
+//     word = n;
+//   }
+//   console.log(word);
+// }
+// version2 shortcut
+
+for (let n = 1; n <= 100; n ++){
+  let word = "";
+  if (n % 3 == 0) word += "Fizz";
+  if (n % 5 == 0) word += "Buzz";
+  console.log(word || n);
 }
