4fa58804edbf8ab4efb78ee61ef70876850aebe2
SANPAK Ng <pakeng369@gmail.com>
2019-09-30 21:42:02 -0400

    JOURNAL

--- data_structure.js
+++ data_structure.js
@@ -84,4 +84,61 @@ function tableFor(event,journal) {
   }
   return table;
 }
+
+function phi(table) {
+  return (table[3] * table[0] - table[2] * table[1]) /
+  Math.sqrt((table[2] + table[3]) *
+            (table[0] + table[1]) *
+            (table[1] + table[3]) *
+            (table[0] + table[2]));
+
+}
+
+//writing my own function
+
+// function eventJorunal(jorunal){
+//   let events = [];
+//   for (let entry of journal) {
+//     for (let event of entry.events ) {
+//       if (!events.includes(event)) events.push(event);
+//     }
+//   }
+// }
+//   return events;
+// }
+
 console.log(tableFor("pizza",JOURNAL));
+
+for (let entry of JOURNAL) {
+  console.log(`${entry.events.length} events.`);
+}
+
+function journalEvents(journal) {
+  let events = [];
+  for (let entry of journal) {
+    for (let event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    }
+  }
+  return events;
+}
+
+// console.log(journalEvents(JOURNAL));
+// for (let event of journalEvents(JOURNAL)) {
+//   console.log(event + ":", phi(tableFor(event,JOURNAL)));
+// }
+
+for (let event of journalEvents(JOURNAL)) {
+  let correlation = phi(tableFor(event,JOURNAL))
+  if (correlation > 0.1 || correlation < -0.1 ) console.log(event + ":" ,correlation);
+}
+
+for (let entry of JOURNAL) {
+  if (entry.events.includes("peanuts") &&
+      !entry.events.includes("brushed teeth")) {
+        entry.events.push("peanut teeth");
+      }
+}
+console.log(phi(tableFor("peanut teeth",JOURNAL)));
