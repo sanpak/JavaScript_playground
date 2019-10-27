779af8ff0645ad3a7c6e6c191a4e7f34553a1bca
SANPAK Ng <pakeng369@gmail.com>
2019-10-27 17:34:41 -0400

    nth recursive and literal

--- listToArray.js
+++ listToArray.js
@@ -32,4 +32,21 @@ function prepend(element,list=null) {
 
   return {value: element, rest:list};
 }
-console.log(prepend(10,prepend(20,null)));
+// console.log(prepend(10,prepend(20,null)));
+
+function nth_rescursive(list,n){
+  if (n == 0){
+    return list.value;
+  }
+  return nth_rescursive(list.rest,n - 1);
+}
+
+function nth(list,n){
+  let elements = list;
+  for (i = 0; i < n; i++) {
+    elements = elements.rest;
+  }
+  return elements.value;
+}
+console.log(nth(arrayToList([10, 20, 30, 40]), 0));
+// console.log(nth_rescursive(arrayToList([10, 20, 30, 40]), 2));
