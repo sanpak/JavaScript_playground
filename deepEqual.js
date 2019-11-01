55e0abcc955525af3c6f53929a5fd388e1555c86
SANPAK Ng <pakeng369@gmail.com>
2019-10-31 23:35:31 -0400

    deep Equal trying  hard

--- deepEqual.js
+++ deepEqual.js
@@ -1,6 +1,59 @@
-let obj = {here: {is: "an"}, object: 2};
+let obj1 = {here: {is: "an"}, object: 2};
 let obj2 = {here: {is: "an"}, object: 2};
 
+function findValue(obj) {
+  let objkeys = Object.keys(obj);
+  let objValues = Object.values(obj);
+
+  for (let value of objValues) {
+    if (typeof(value) == "object") {
+      return findValue(value);
+    }
+  }
+  return objValues;
+}
+
+// console.log(findValue(obj1));
+
+// function deepEqual(obj1,obj2){
+//
+//   let obj1keys = Object.keys(obj1);
+//   let obj1Values = Object.values(obj1);
+//   let obj2keys = Object.keys(obj2);
+//   let obj2Values = Object.values(obj2);
+//
+//   if (obj1keys.length != obj2keys.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < obj1keys.length; i++) {
+//     if (obj1keys[i] != obj2keys[i]) {
+//       return false;
+//     }
+//   }
+//   for (let i = 0; i < obj1Values.length; i++) {
+//     if (obj1Values[i] != obj2Values[i]) {
+//       return false;
+//     }
+//   }
+//
+//   for (let i = 0; i < obj1Values.length; i++ ) {
+//     if (typeof(obj1Values[i]) == "object" && typeof(obj2Values[i]) == "object"){
+//       deepEqual(obj1Values[i],obj2Values[i]);
+//     } else if (typeof(obj1Values[i]) == "object" || typeof(obj2Values[i]) == "object") {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+
+// console.log(deepEqual(obj1,obj2));
+
+// console.log(findObjValue(obj));
+
+// function deepEqual(obj1,obj2){
+//
+// }
 
 // function deepEqual(obj1,obj2) {
 //   // if (typeof(obj1) != "object" || typeof(obj2) != "object" ) {
@@ -47,4 +100,4 @@ let obj2 = {here: {is: "an"}, object: 2};
 //   // return true;
 // }
 
-console.log(deepEqual(obj,obj2));
+// console.log(deepEqual(obj,obj2));
