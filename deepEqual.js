df0e4eba54b41ad350ca51eb893eb78f4fa4e522
SANPAK Ng <pakeng369@gmail.com>
2019-11-02 17:22:07 -0400

    looked up the solution

--- deepEqual.js
+++ deepEqual.js
@@ -1,56 +1,75 @@
-let obj1 = {here: {is: "an"}, object: 2};
-let obj2 = {here: {is: "an"}, object: 2};
+let obj = {here: 1, object: {a: {b: 1}}};
+let obj2 = {here: 1, object: {a: {b: 1}}};
 
-console.log(obj1 === obj2);
+console.log(typeof({a:1}));
+function deepEqual(a,b) {
+  if (a === b) return true;
 
-function findValue(obj) {
-  let objkeys = Object.keys(obj);
-  let objValues = Object.values(obj);
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object") return false;
 
-  for (let value of objValues) {
-    if (typeof(value) == "object") {
-      return findValue(value);
-    }
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key],b[key])) return false;
   }
-  return objValues;
+
+  return true;
 }
 
-// console.log(findValue(obj1));
+console.log(deepEqual(obj,obj2));
 
-function deepEqual(obj1,obj2) {
-  let result = true;
-  let obj1keys = Object.keys(obj1);
-  let obj2keys = Object.keys(obj2);
-  let obj1Values = Object.values(obj1);
-  let obj2Values = Object.values(obj2);
 
-  if (obj1keys.length != obj2keys.length) {
-    return false;
-  }
-  for (let i = 0; i < obj1Values.length; i++) {
-    if (typeof(obj1Values[i]) != "object" && typeof(obj2Values[i]) != "object") {
-      if (obj1Values[i] != obj2Values[i]) {
-        return false;
-      }
-    }
-  }
-  for (let i = 0; i < obj1Values.length; i++) {
-    if (typeof(obj1Values[i]) == "object" && typeof(obj2Values[i]) == "object") {
-      result = deepEqual(obj1Values[i],obj2Values[i]);
-      //test
-      // console.log(obj1Values[i],obj2Values[i]);
-    }
+// function findValue(obj) {
+//   let objkeys = Object.keys(obj);
+//   let objValues = Object.values(obj);
+//
+//   for (let value of objValues) {
+//     if (typeof(value) == "object") {
+//       return findValue(value);
+//     }
+//   }
+//   return objValues;
+// }
+//
+// // console.log(findValue(obj1));
+//
+// function deepEqual(obj1,obj2) {
+//   let result = true;
+//   let obj1keys = Object.keys(obj1);
+//   let obj2keys = Object.keys(obj2);
+//   let obj1Values = Object.values(obj1);
+//   let obj2Values = Object.values(obj2);
+//
+//   if (obj1keys.length != obj2keys.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < obj1Values.length; i++) {
+//     if (typeof(obj1Values[i]) != "object" && typeof(obj2Values[i]) != "object") {
+//       if (obj1Values[i] != obj2Values[i]) {
+//         return false;
+//       }
+//     }
+//   }
+//   for (let i = 0; i < obj1Values.length; i++) {
+//     if (typeof(obj1Values[i]) == "object" && typeof(obj2Values[i]) == "object") {
+//       result = deepEqual(obj1Values[i],obj2Values[i]);
+//       //test
+//       // console.log(obj1Values[i],obj2Values[i]);
+//     }
+//
+//   }
+//
+//   // console.log(obj1Values[0],obj2Values[0]);
+//   if (obj1Values[0] != obj2Values[0]) {
+//     return false;
+//   }
+//   return result;
+// }
 
-  }
-
-  // console.log(obj1Values[0],obj2Values[0]);
-  if (obj1Values[0] != obj2Values[0]) {
-    return false;
-  }
-  return result;
-}
-
-console.log(deepEqual(obj1,obj2));
+// console.log(deepEqual(obj1,obj2));
 
 // console.log(JSON.parse(string).);
 
