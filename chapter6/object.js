a4ef0603257e1daa4434fcebf5474b92b5696764
SANPAK Ng <pakeng369@gmail.com>
2019-11-17 22:39:13 -0500

    object prototype

--- chapter6/object.js
+++ chapter6/object.js
@@ -8,5 +8,16 @@ function speak(line) {
 }
 let whiteRabbit = {type: "white", speak};
 let hungryRabbit = {type: "hungry", speak};
-whiteRabbit.speak("oh my ears and whiskers, " + "how late it's getting!");
-hungryRabbit.speak("asdasd");
+// whiteRabbit.speak("oh my ears and whiskers, " + "how late it's getting!");
+// hungryRabbit.speak("asdasd");
+//
+// speak.call(hungryRabbit, "Burp!");
+// speak.call(whiteRabbit,"ajsdhakjshd");
+
+function normalize() {
+  console.log(this.coords.map(n => n / this.length));
+}
+normalize.call({coords: [0,2,3], length: 5});
+let empty = {};
+console.log(empty.toString);
+console.log(empty.toString());
