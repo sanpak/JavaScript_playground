2699a72217343823a24a3e8cb7238c4eebb09442
SANPAK Ng <pakeng369@gmail.com>
2019-11-24 23:25:41 -0500

    constractor ptototype

--- chapter6/object.js
+++ chapter6/object.js
@@ -19,13 +19,58 @@ function normalize() {
 }
 normalize.call({coords: [0,2,3], length: 5});
 let empty = {};
-console.log(empty.toString);
-console.log(empty.toString());
 
-console.log(Object.getPrototypeOf({}) == Object.prototype);
+//
+// console.log(Object.getPrototypeOf({}) == Object.prototype);
+//
+// console.log(Object.getPrototypeOf({}));
+//
+// console.log(Object.getPrototypeOf("asd"));
+// console.log(Object.prototype);
+// console.log({}.toString);
+// console.log(Object.getPrototypeOf(Math.max) == Function.prototype);
+// console.log(Object.getPrototypeOf(Math.max));
+// console.log(Object.getPrototypeOf([]));
+//
+//
+// console.log(empty.toString);
+// console.log(empty.toString());
+// console.log(Object.prototype.toString);
+let protoRabbit = {
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+};
 
-console.log(Object.getPrototypeOf({}));
+// let killerRabbit = Object.create(protoRabbit);
+// killerRabbit.type = "Killer";
+// killerRabbit.speak("SKREEE!");
 
-console.log(Object.getPrototypeOf("asd"));
-console.log(Object.prototype);
-console.log({}.toString);
+function makeRabbit(type) {
+  let rabbit = Object.create(protoRabbit);
+  rabbit.type = type;
+  return rabbit;
+}
+
+console.log(makeRabbit("killerRabbit"));
+
+function Rabbit(type) {
+  this.type = type;
+}
+
+Rabbit.prototype.speak = function(line) {
+  console.log(`The ${this.type} rabbit says '${line}'`);
+};
+
+// Rabbit.speak = function(line) {
+//   console.log(`The ${this.type} rabbit says '${line}'`);
+// };
+
+let weirdRabbit = new Rabbit("weird");
+console.log(Object.getPrototypeOf(weirdRabbit));
+console.log(weirdRabbit.speak("hello"));
+console.log(weirdRabbit);
+
+console.log(Object.getPrototypeOf(Rabbit) == Function.prototype);
+console.log(Object.getPrototypeOf(Rabbit));
+console.log(Object.getPrototypeOf(weirdRabbit));
