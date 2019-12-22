dd797f1d91b37a7ebe2c785a0d4b92d94eeca6da
SANPAK Ng <pakeng369@gmail.com>
2019-12-22 13:41:29 -0500

    to string() experiment

---
+++ chapter6/map.js
@@ -0,0 +1,14 @@
+let ages = {
+  Boris: 39,
+  Liang: 22,
+  Julia: 62
+};
+
+console.log(`Julia is ${ages["Julia"]}`);
+console.log("Is Jack's age know?", "Jack" in ages);
+console.log("Is Jack's age know?", "Jack" in ages);
+console.log("John" in ages);
+console.log("toString" in ages);
+console.log(Object.getPrototypeOf("toString"));
+
+var 
