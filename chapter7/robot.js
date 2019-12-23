da5dabb8e52c77072c69ca6a6d833a1a52d57c39
SANPAK Ng <pakeng369@gmail.com>
2019-12-22 20:17:22 -0500

    Robot simulation

--- chapter7/robot.js
+++ chapter7/robot.js
@@ -7,3 +7,70 @@ const roads = [
   "Marketplace-Post Office",      "Marketplace-Shop",
   "Marketplace-Town Hall",        "Shop-Town Hall"
 ];
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from,to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from,to] of edges.map(r => r.split("-"))) {
+    addEdge(from,to);
+    addEdge(to,from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+console.log(roadGraph);
+
+
+class VillageState {
+  constructor(place,parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != this.place) return p;
+        return {place: destination, address: p.address};
+      }).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+let first = new VillageState(
+  "Post Office",
+  [{place: "Post Office", address: "Alice's House"}]
+);
+
+let next = first.move("Alice's House");
+console.log(next.place);
+console.log(next.parcels);
+console.log(first.place);
+
+let object = Object.freeze({value: 5});
+object.value = 10;
+console.log(object.value);
+
+// console.log(next.place);
