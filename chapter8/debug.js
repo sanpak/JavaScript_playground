44795c8bbcc897bb5831b241a883c9919bec31d6
san <hillsfrontendweb@gmail.com>
2020-03-31 20:12:33 -0400

    type

--- chapter8/debug.js
+++ chapter8/debug.js
@@ -1,6 +1,18 @@
-funciton canYouSpotTheProblem() {
-  "use strict";
-  for (counter = 0; counter < 10; counter++){
-    console.log("Happy happy");
-  }
+// funciton canYouSpotTheProblem() {
+//   "use strict";
+//   for (counter = 0; counter < 10; counter++){
+//     console.log("Happy happy");
+//   }
+// }
+
+// function Person(name) { this.name = name; }
+// let ferdinand = Person("Ferdinand");
+// console.log(ferdinand);
+// console.log(name);
+"use strict";
+function Person(name) { this.name = name; }
+let ferdinand = Person("Ferdinand");
+// (VillageState, Array) -> {direction: string, memory: Array}
+function goalOrientedRobot(state, memory){
+  //...
 }
