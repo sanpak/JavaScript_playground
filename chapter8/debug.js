5f21d450eeae467d7aa4b5d4a7069d093e97b953
san <hillsfrontendweb@gmail.com>
2020-04-05 16:50:09 -0400

    debugger

--- chapter8/debug.js
+++ chapter8/debug.js
@@ -9,10 +9,37 @@
 // let ferdinand = Person("Ferdinand");
 // console.log(ferdinand);
 // console.log(name);
-"use strict";
-function Person(name) { this.name = name; }
-let ferdinand = Person("Ferdinand");
-// (VillageState, Array) -> {direction: string, memory: Array}
-function goalOrientedRobot(state, memory){
-  //...
+// "use strict";
+// function Person(name) { this.name = name; }
+// let ferdinand = Person("Ferdinand");
+// // (VillageState, Array) -> {direction: string, memory: Array}
+// function goalOrientedRobot(state, memory){
+//   //...
+// }
+//test example
+// function test(label, body) {
+//   if (!body()) console.log('Failed: ${label}');
+// }
+// test("convert Latin text to uppercase",() => {
+//   return "hello".toUpperCase() == "HELLO";
+// });
+// test("convert Greek text to uppercase",() => {
+//   return "Χαίρετε".toUpperCase() == "ΧΑΊΡΕΤΕ";
+// })
+//
+// test("don't convert case-less characters", () => {
+//   return "مرحبا".toUpperCase() == "مرحبا";
+// });
+function numberToString(n, base = 10) {
+  let result = "", sign = "";
+  if (n < 0){
+    sign = "-";
+    n = -n;
+  }
+  do {
+    result = String(n % base) + result;
+    n = Math.floor(n / base);
+  } while (n > 0);
+  return sign + result;
 }
+console.log(numberToString(12));
