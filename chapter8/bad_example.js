68a9a407991a65342568598855cefe29b330f933
san <hillsfrontendweb@gmail.com>
2020-04-09 17:50:06 -0400

    bad example

---
+++ chapter8/bad_example.js
@@ -0,0 +1,18 @@
+const accounts = {
+  a: 100,
+  b: 0,
+  c: 20
+};
+
+function getAccount() {
+  let accountName = prompt("Enter an account name");
+  if (!accounts.hasOwnProperty(accountName)) {
+    throw new Error('No such account: ${accountName}');
+  }
+  return accountName;
+}
+function transfer(from,amount) {
+  if (accounts[from] < amount) return;
+  accounts[from] -= amount;
+  accounts[getAccount()] += amount;
+}
