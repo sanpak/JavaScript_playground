cd6ae7704ba2540500db2eaab3c3298128d8f654
san <hillsfrontendweb@gmail.com>
2020-04-10 11:50:30 -0400

    finally

---
+++ chapter8/finally.js
@@ -0,0 +1,14 @@
+function transfer(from, amount) {
+  if (accounts[from] < amount) return;
+  let progress = 0;
+  try {
+    accounts[from] -= amount;
+    progress = 1;
+    accounts[getAccount()] += amount;
+    progress = 2;
+  } finally {
+    if (progress == 1) {
+      accounts[from] += amount;
+    }
+  }
+}
