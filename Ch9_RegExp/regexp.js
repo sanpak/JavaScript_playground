77d2e1737e7ae798c1f6a58c4b99eab9691bf162
san <hillsfrontendweb@gmail.com>
2020-04-15 17:25:58 -0400

    star Reg Exp

--- Ch9_RegExp/regexp.js
+++ Ch9_RegExp/regexp.js
@@ -10,5 +10,12 @@
 //
 // let testReg = /\d::/;
 // console.log(testReg.test("5::"));
-let notBinary = /[^01]/;
-console.log(notBinary.test("11000000001000000001000000001000000001000000001000000002"));
+// let notBinary = /[^01]/;
+// console.log(notBinary.test("11000000001000000001000000001000000001000000001000000002"));
+// console.log(/'\d+'/.test("'123'"));
+// console.log(/'\d+'/.test("''"));
+// console.log(/'\d*'/.test("'123'"));
+// console.log(/'\d*'/.test("''"));
+console.log("___________try star____________");
+let testStar = /d*/;
+console.log(testStar.test("asdfasdf"));
