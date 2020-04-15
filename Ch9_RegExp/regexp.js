fa8d69b2d6232fd92c0ffe867f5546ed555ac6ea
san <hillsfrontendweb@gmail.com>
2020-04-15 16:41:53 -0400

    caret

--- Ch9_RegExp/regexp.js
+++ Ch9_RegExp/regexp.js
@@ -1,8 +1,14 @@
-console.log(/abc/.test("abcde"));
-
-console.log(/abc/.test("abxde"));
-
-console.log(/[0-9]/.test("in 1992"));
-
-let dateTime = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/;
-console.log(dateTime.test("01-30-2003 15:20"));
+// console.log(/abc/.test("abcde"));
+//
+// console.log(/abc/.test("abxde"));
+//
+// console.log(/[0-9]/.test("in 1992"));
+//
+// let dateTime = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/;
+// console.log(dateTime.test("01-30-2003 15:20"));
+// console.log(dateTime.test("01-jan-2003 15"))
+//
+// let testReg = /\d::/;
+// console.log(testReg.test("5::"));
+let notBinary = /[^01]/;
+console.log(notBinary.test("11000000001000000001000000001000000001000000001000000002"));
