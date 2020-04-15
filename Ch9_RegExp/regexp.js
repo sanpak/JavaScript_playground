b4e03ea6e093473015873ce1ba5a84d05e95ae13
san <hillsfrontendweb@gmail.com>
2020-04-15 16:04:56 -0400

    special characters

---
+++ Ch9_RegExp/regexp.js
@@ -0,0 +1,8 @@
+console.log(/abc/.test("abcde"));
+
+console.log(/abc/.test("abxde"));
+
+console.log(/[0-9]/.test("in 1992"));
+
+let dateTime = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/;
+console.log(dateTime.test("01-30-2003 15:20"));
