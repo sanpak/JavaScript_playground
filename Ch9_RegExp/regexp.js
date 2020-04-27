d587e435e877a5404d75c6c2c57127f4fc7f3455
san <hillsfrontendweb@gmail.com>
2020-04-26 20:36:22 -0400

    non greedy

--- Ch9_RegExp/regexp.js
+++ Ch9_RegExp/regexp.js
@@ -115,3 +115,7 @@ function stripComments(code) {
 console.log(stripComments("1 + /* 2 */3"));
 console.log(stripComments("x = 10; // ten!"));
 console.log(stripComments("1 /* a */+/* b */ 1"));
+function stripComments(code) {
+  return code.replace(/\/\/.*|\/\*[^]*?\*\//g, "");
+}
+console.log(stripComments("1 /* a */+/* b */ 1"));
