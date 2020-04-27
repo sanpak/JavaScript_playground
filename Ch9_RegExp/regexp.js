c5e39a73a57f86ac2c0e057fcb8d6cec8242e5d2
san <hillsfrontendweb@gmail.com>
2020-04-26 20:33:55 -0400

    strip comments

--- Ch9_RegExp/regexp.js
+++ Ch9_RegExp/regexp.js
@@ -89,22 +89,29 @@
 // console.log("______________backtrack example___________");
 // exp = /(x+y+)+y/;
 // console.log(exp.test("xxxxxxxxxxy"));
-console.log("______________replace_______________");
-console.log("papa".replace("p","m"));
-console.log("Borobudur".replace(/[ou]/,"a"));
-console.log("Borobudur".replace(/[ou]/g,"a"));
-console.log("Liskov, Barbara\nMcCarthy, John\nWadler, Philip".replace(/(\w+), (\w+)/g, "$2 $1"));
-let s = "the cia and fbi";
-console.log(s.replace(/\b(fbi|cia)\b/g, str => str.toUpperCase()));
-let stock = "1 lemon, 2 cabbages, and 101 eggs";
-function minusOne(match, amount, unit) {
-  amount = Number(amount) - 1;
-  if (amount == 1) {
-    unit = unit.slice(0, unit.length - 1);
-  } else if (amount == 0) {
-    amount = "no";
-  }
-  return amount + " " + unit;
+// console.log("______________replace_______________");
+// console.log("papa".replace("p","m"));
+// console.log("Borobudur".replace(/[ou]/,"a"));
+// console.log("Borobudur".replace(/[ou]/g,"a"));
+// console.log("Liskov, Barbara\nMcCarthy, John\nWadler, Philip".replace(/(\w+), (\w+)/g, "$2 $1"));
+// let s = "the cia and fbi";
+// console.log(s.replace(/\b(fbi|cia)\b/g, str => str.toUpperCase()));
+// let stock = "1 lemon, 2 cabbages, and 101 eggs";
+// function minusOne(match, amount, unit) {
+//   amount = Number(amount) - 1;
+//   if (amount == 1) {
+//     unit = unit.slice(0, unit.length - 1);
+//   } else if (amount == 0) {
+//     amount = "no";
+//   }
+//   return amount + " " + unit;
+// }
+//
+// console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+console.log("___________greed____________");
+function stripComments(code) {
+  return code.replace(/\/\/.*|\/\*[^]*\*\//g, "");
 }
-
-console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+console.log(stripComments("1 + /* 2 */3"));
+console.log(stripComments("x = 10; // ten!"));
+console.log(stripComments("1 /* a */+/* b */ 1"));
